dba71979040f0d70e0fbb0848495eb46cafc9db6
willy <shen.willy95@gmail.com>
2021-06-18 14:11:39 +0700

    husky fix

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -13,7 +13,8 @@ contract Counter {
     function countUp() public returns (uint256) {
         console.log("countUp: count =", count);
         uint256 newCount = count + 1;
-        require(newCount > count, "Uint256 overflow");count = newCount;
+        require(newCount > count, "Uint256 overflow");
+        count = newCount;
         emit CountedTo(count);
         return count;
     }
